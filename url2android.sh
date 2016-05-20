0925d5e02a5043cf24c75fcda31e5b763672ef48
Daryn St. Pierre <daryn@bigseadesign.com>
2016-05-20 08:35:40 -0400

    Updated the comments in each shell script for clarity.

--- url2android.sh
+++ url2android.sh
@@ -2,11 +2,16 @@
 # Load a URL in Google Chrome
 #
 if [ "$@" ]; then
+
   # Strip http or https from the URL prior because
-  # we want to handle that here.
+  # we want to handle that here. Not including the http://
+  # makes the browser open up blank, so this ensures that
+  # doesn't happen.
   url="$( echo "$@" | sed -e 's#^http://##; s#^https://##' )"
+
   # Push the desired URL to Google Chrome on Android
   adb shell am start -n com.android.chrome/com.google.android.apps.chrome.Main -d http://$url
+
 else
   echo "No URL specified!";
 fi
--- url2silk.sh
+++ url2silk.sh
@@ -2,11 +2,16 @@
 # Load a URL in Silk
 #
 if [ "$@" ]; then
+
   # Strip http or https from the URL prior because
-  # we want to handle that here.
+  # we want to handle that here. Not including the http://
+  # makes the browser open up blank, so this ensures that
+  # doesn't happen.
   url="$( echo "$@" | sed -e 's#^http://##; s#^https://##' )"
+
   # Push the desired URL to Silk on Amazon Fire tablets
   adb shell am start -n com.amazon.cloud9/.browsing.BrowserActivity -d http://$url
+
 else
   echo "No URL specified!";
 fi
